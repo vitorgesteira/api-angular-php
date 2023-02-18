4f95653719984197dd4f7806765118c0a0ceeefd
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2023-02-17 21:26:36 -0300

    aquivo listar os cursos da tabela

---
+++ php/listar.php
@@ -0,0 +1,33 @@
+<?php 
+
+//Incluir a conexão
+include("conexao.php");
+
+//SQL
+$sql = "SELECT * FROM cursos";
+
+//Executar
+$executar = mysqli_query($conexao, $sql);
+
+//Vetor
+$cursos = [];
+
+//Indice
+$indice = 0;
+
+//Laço
+while($linha = mysqli_fetch_assoc($executar)){
+    $cursos[$indice]['idCurso'] = $linha['idCurso'];
+    $cursos[$indice]['nomeCurso'] = $linha['nomeCurso'];
+    $cursos[$indice]['valorCurso'] = $linha['valorCurso'];
+
+    $indice++;
+}
+
+//JSON
+json_encode(['cursos'=>$cursos]);
+
+//teste
+var_dump($cursos);
+
+?>
